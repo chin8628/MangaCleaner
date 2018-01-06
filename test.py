b6b18662bc35e777685870a146302eff879eb44c
chin8628 <boon.8628@gmail.com>
2018-01-07 02:36:26 +0700

    Update

--- test.py
+++ test.py
@@ -1,8 +1,34 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread('3.jpg')
-print (img.shape[:2])
-a = np.zeros((3,2))
+def showImg(title, img, flag=0):
+    f, ax = plt.subplots()
+    f.canvas.set_window_title(title)
 
-print (a)
+    if flag == 0:
+        ax.imshow(img, interpolation = 'none')
+    elif flag == 1:
+        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), interpolation = 'none')
+    elif flag == 2:
+        ax.imshow(img, cmap='gray', interpolation = 'none')
+
+img_src = cv2.imread('test.jpg')
+
+th, im_th = cv2.threshold(img_src, 220, 255, cv2.THRESH_BINARY_INV);
+h, w = im_th.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+cv2.floodFill(im_th, mask, (50,50), 255, None, None, cv2.FLOODFILL_MASK_ONLY)
+
+im2, contours, hierarchy = cv2.findContours(image=mask, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+img_out = img_src.copy()
+cv2.drawContours(
+    image=img_out, \
+    contours=contours, \
+    contourIdx=-1, \
+    color=(0,0,255), \
+    thickness=1)
+
+
+showImg("Test", img_out, 2)
+plt.show()
