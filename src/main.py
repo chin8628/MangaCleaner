69d3b20f1495f2d96f032f6308c8146640e339c3
chin8628 <boonyarith.pir@gmail.com>
2018-07-26 14:42:05 +0900

    Added text detection and main script

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import fire
+import text_detection
+
+class Main(object):
+    """A simple calculator class."""
+
+    def text_detection(self, path):
+        # ToDo: Implement the real text detection module
+        print(path)
+
+if __name__ == '__main__':
+    fire.Fire(Main)
