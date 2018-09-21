046922f70f7a2d773d18eba44b51f1890cd2a94b
chin8628 <boonyarith.pir@gmail.com>
2018-09-21 19:02:21 +0900

    Added extracting for testing

--- src/create_batch.py
+++ src/create_batch.py
@@ -4,15 +4,14 @@ from modules.utils import chunks
 dataset_img_path = '../../danbooru/resized/images/'
 
 commands = map(
-    lambda x: 'python save_label.py % s ../output/%s.json\n' % (x.split('.')[0], x.split('.')[0]),
+    lambda x: 'python testing_extract.py % s ../output/%s.json\n' % (x.split('.')[0], x.split('.')[0]),
     os.listdir(dataset_img_path)
 )
-cmd_chunks = chunks(list(commands), 6)
+cmd_chunks = chunks(list(commands), 159)
 
 cnt = 0
 for cmd_chunk in cmd_chunks:
-    with open('../batch/%d.sh' % cnt, 'w') as fp:
-        fp.write("'#!/bin/bash'\n")
+    with open('../batch/%d-testing.sh' % cnt, 'w') as fp:
         fp.writelines(cmd_chunk)
     fp.close()
 
