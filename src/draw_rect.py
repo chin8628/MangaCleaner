15c13bc46c95bbfe55f4b08f592bb22b205d1b61
chin8628 <boonyarith.pir@gmail.com>
2018-09-21 20:24:25 +0900

    Added plot_hist and draw_rect

--- src/draw_rect.py
+++ src/draw_rect.py
@@ -5,12 +5,21 @@ from modules.file_manager import load_dataset
 from modules.label_rect import label
 
 
-def draw_rect(id, dataset_path):
+def draw_rect(id, dataset_path, file_type):
     data = load_dataset(dataset_path)
     src = cv2.imread('../../danbooru/resized/images/%s.jpg' % id)
 
+    if file_type == 'train':
+        is_text_for_show = [1, 0]
+    elif file_type == 'test':
+        is_text_for_show = [-1]
+    elif file_type == 'predicted':
+        is_text_for_show = [1]
+
+    print(is_text_for_show)
+
     widths, heights, topleft_pts = [], [], []
-    for datum in list(filter(lambda x: x['is_text'] in [-1, 1, 0], data)):
+    for datum in list(filter(lambda x: x['is_text'] in is_text_for_show, data)):
         topleft_pts.append((datum['topleft_pt']['y'], datum['topleft_pt']['x']))
         widths.append(datum['width'])
         heights.append(datum['height'])
--- src/plot_hist.py
+++ src/plot_hist.py
@@ -3,15 +3,14 @@ import cv2
 from modules.file_manager import load_dataset
 from tqdm import tqdm
 
-title = 'LoveHina_vol01'
-page = 30
-img = cv2.imread('../../Dataset_Manga/Manga109/images/%s/%03d.jpg' % (title, page))
-data = load_dataset('../output/%s-%03d.json' % (title, page))
+title = '1164695'
+img = cv2.imread('../../danbooru/resized/images/%s.jpg' % (title))
+data = load_dataset('../output/%s.json' % (title))
 
 for datum in tqdm(data):
-    # plt.bar(range(0, 256), datum['hist'])
-    # plt.savefig('../output_hist/{}/{}-hist.png'.format(datum['is_text'], datum['id']))
-    # plt.close()
+    plt.bar(range(0, 256), datum['hist'])
+    plt.savefig('../output_hist/{}/{}-hist.png'.format(datum['is_text'], datum['id']))
+    plt.close()
 
     cv2.imwrite('../output_hist/{}/{}.jpg'.format(datum['is_text'], datum['id']), img[
         datum['topleft_pt']['y']:datum['topleft_pt']['y'] + datum['height'],
