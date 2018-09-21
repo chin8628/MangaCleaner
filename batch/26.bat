5cf8f969893117d42667c84db4610735990b09bc
chin8628 <boonyarith.pir@gmail.com>
2018-09-21 13:35:00 +0900

    Update batch

--- batch/26.bat
+++ batch/26.bat
@@ -1,3 +1,3 @@
-python draw_rect.py 991063 ../output/991063.json
-python draw_rect.py 991067 ../output/991067.json
-python draw_rect.py 991072 ../output/991072.json
+python save_label.py 991063 ../output/991063.json
+python save_label.py 991067 ../output/991067.json
+python save_label.py 991072 ../output/991072.json
